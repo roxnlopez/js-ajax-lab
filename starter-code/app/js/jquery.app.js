83449d4855b22c62dee96ed9c5584eb06853af36
roxnlopez <roxnlopez@gmail.com>
2017-07-18 19:53:04 -0600

    shit

--- starter-code/app/js/jquery.app.js
+++ starter-code/app/js/jquery.app.js
@@ -1,39 +1,62 @@
-//Prevent default form submission
-$('#new-cat').on('submit', function(event){
-	console.log("clicked");
-	event.preventDefault();
-	//push to api
-});
+$(document).ready(function(){
+	console.log('ready');
 
+	//Prevent default form submission
+	$('#new-cat').on('submit', function(){
+			console.log("clicked");
 
+		//push to api
+	});
 
-//Collected forms data into object
+	//Display API contents in list below forms
+		//#cats(ul)
+	// <li>obj.name + " - " + obj.note</li>
+	$.ajax({
+		type: 'GET',
+		url: 'https://ga-cat-rescue.herokuapp.com/api/cats',
+		dataType: 'JSON',
+		success: (function (data) {
+			data.forEach(function(el) {
+				$('#cats').append("<li>" + el.name + "-" + el.note + "</li>");
+			});
+		})
 
-	// #cat-name
-	// #cat-note
+	});
+//cat list is there, now add preventdefault so it doesn't refresh
+	$("form").on("submit", function(event) {
+		event.preventDefault();
 
+	var newCat = $('#cat-name').val();
+	var newNote = $('#cat-note').val();
 
+	var newCatObject = {
+		name: newCat,
+		note: newNote
+	};
+	console.log(newCatObject);
+	//console log that shit yo
 
+	$.post('https://ga-cat-rescue.herokuapp.com/api/cats', JSON.stringify(newCatObject))
+			.done(function (data) {
+			 var newCatData = JSON.parse(data);
+        $('#cats').append('<li>' + newCatData.name + ' - <em>' + newCatData.note + '</em></li>');
+      }); 
 
-//Send object to API
-	//https://ga-cat-rescue.herokuapp.com/api/cats
+			console.log('Roxann');
+	});
+//create some new cool cats
+//$('.results').append("Once upon a time there was a Developer named " + $("#name").val().split(" ").join("") + ". "
+	
+//create object with new info 
 
-$.post('https://ga-cat-rescue.herokuapp.com/api/cats', '{"name": " ", "note": " "}');
-
-
-
-
-//Display API contents in list below forms
-	//#cats(ul)
-// <li>obj.name + " - " + obj.note</li>
-$.ajax({
-	type: 'GET',
-	url: 'https://ga-cat-rescue.herokuapp.com/api/cats',
-	dataType: 'JSON',
-	success: function (data) {
-		data.forEach(function(el) {
-			$('#cats').append("<li>" + el.name + "-" + el.note + "</li>");
-		});
-	}
-
+	
+	//add cat or note and refresh list
+	//$('#new-name').serialize();
+	//$.ajax({
+	//	type: 'POST',
+	//	url: 'https://ga-cat-rescue.herokuapp.com/api/cats',
+	//	data: $('#new-name').serialize(),
+	//	success: function(el){
+	//		console.log('hi');
+	//	}
 });
